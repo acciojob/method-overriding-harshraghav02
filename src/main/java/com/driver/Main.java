620c324c6328282bd488e0e54a15f985f1323d6b
Harsh <raghavharsh88@gmail.com>
2023-03-19 11:38:15 +0530

    Updated Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,14 +10,4 @@ public class Main {
 
     }
 }
-class A {
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-    @Override
-    public String meth(){
-        return "Method is overridden in Extendend class B";
-    }
-}
+
